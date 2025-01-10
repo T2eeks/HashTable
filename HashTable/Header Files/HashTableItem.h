b29d858857087088ba8905814a38c8266ad136f6
Artyom <artemrogozinzza@gmail.com>
2025-01-11 04:24:21 +0700

    Comments

--- HashTable/Header Files/HashTableItem.h
+++ HashTable/Header Files/HashTableItem.h
@@ -1,12 +1,22 @@
 #pragma once
 #include <string>
 
+//! \brief Структура элемента хеш-таблицы
+//! \details Представляет собой узел в односвязном списке для разрешения коллизий методом цепочек
 struct HashTableItem
 {
+    //! \brief Получить ключ элемента
     std::string Key;
+    //! \brief Получить значение элемента
     std::string Value;
+
+    //! \brief Указатель на следующий элемент в списке
     HashTableItem* Next;
 
+    //! \brief Конструктор элемента хеш-таблицы
+    //! \param key Ключ элемента
+    //! \param value Значение элемента
+    //! \param next Указатель на следующий элемент в списке 
     HashTableItem(const std::string& key, const std::string& value, HashTableItem* next = nullptr)
         : Key(key), Value(value), Next(next) {}
 };
